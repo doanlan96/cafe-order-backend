4e0d9b151874671a158c8c114e1afa916431424f
Pham Duc <ducpham01626064102@gmail.com>
2022-03-04 10:57:17 +0700

    fix create-update product

--- src/main/java/com/thi/controller/ProductController.java
+++ src/main/java/com/thi/controller/ProductController.java
@@ -19,12 +19,13 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.thi.entity.Category;
 import com.thi.entity.Product;
 import com.thi.service.IProductService;
 
 @RestController
 @RequestMapping(value = "api/v1/products")
-//@CrossOrigin(origins = "http://127.0.0.1:5500")
+@CrossOrigin(origins = "http://localhost:8100")
 public class ProductController {
 	
 	@Autowired
@@ -40,23 +41,36 @@ public class ProductController {
 		return service.getProductByID(id);
 	}
 	
+//	@PostMapping()
+//	public String createProduct(@RequestParam String name, 
+//								@RequestParam MultipartFile picture, 
+//								@RequestParam float price, 
+//								@RequestParam int category_id) throws IOException {
+//		service.createProduct(name, picture, price, category_id);
+//		return "{ \"message\":"+"\"Create Successfully\"" + "}";
+//	}
+	
 	@PostMapping()
-	public String createProduct(@RequestParam String name, 
-								@RequestParam MultipartFile picture, 
-								@RequestParam float price, 
-								@RequestParam int category_id) throws IOException {
-		service.createProduct(name, picture, price, category_id);
-		return "{ \"message\":"+"\"Create Successfully\"" + "}";
+	public Product createProduct(@RequestBody Product product) {
+		service.createProduct(product);
+		return product;
 	}
 
+//	@PutMapping(value = "/{id}")
+//	public void updateProduct(@PathVariable(name = "id") short id,
+//							@RequestParam String name, 
+//							@RequestParam MultipartFile picture, 
+//							@RequestParam float price, 
+//							@RequestParam int category_id) throws IOException {
+////		product.setId(id);
+//		service.updateProduct(id, name, picture, price, category_id);
+//	}
+	
 	@PutMapping(value = "/{id}")
-	public void updateProduct(@PathVariable(name = "id") short id,
-							@RequestParam String name, 
-							@RequestParam MultipartFile picture, 
-							@RequestParam float price, 
-							@RequestParam int category_id) throws IOException {
-//		product.setId(id);
-		service.updateProduct(id, name, picture, price, category_id);
+	public Product updateProduct(@PathVariable(name = "id") short id, @RequestBody Product product) {
+		product.setId(id);
+		service.updateProduct(product);
+		return product;
 	}
 
 	@DeleteMapping(value = "/{id}")
